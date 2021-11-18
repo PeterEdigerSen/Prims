bce7b740f7e086203912143e3383d087fe7972af
peterediger <peter.ediger@delta-v.us>
2021-11-18 08:20:59 +0100

    Correction of 'bug' (mixed column: int and float)

--- src/tests.py
+++ src/tests.py
@@ -8,16 +8,14 @@ start_time_global = time.time()
 for k in range(1, 9):
     n = 10 ** k
     start_time = time.time()
-    n_prime = 0  # n_of_prime_numbers(n)
+    n_prime = n_of_prime_numbers(n)
     runtime = time.time() - start_time
     start_time = time.time()
     n_prime_sen = n_of_prime_numbers_sen(n)
-    print(n_prime_sen)
     runtime_sen = time.time() - start_time
     mydata.append([n, n_prime, n_prime_sen, runtime, runtime_sen])
-
 runtime_global = time.time() - start_time_global
-mydata.append(["Total time", "", runtime_global])
+mydata.append(["Total time", "", "", runtime_global])
 
 head = ["n", "n of prims", "n of prims SEN", "time in sec", "time in sec SEN"]
 print(tabulate(mydata, headers=head, tablefmt="fancy_grid"))
